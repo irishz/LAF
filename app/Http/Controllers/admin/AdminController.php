a9f4200bbde035876739f1abafddb4b53016eeaf
Jetsada Prasitsuwan <ice-jetsada@hotmail.com>
2019-03-22 15:53:01 +0700

    query leave form admin dashboard

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers\admin;
 
-use App\Form;
-use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,11 +11,16 @@ class AdminController extends Controller
 {
     public function index()
     {
-        // $manager_dept = Auth::user()->department;
+        
+        $manager_dept = Auth::user()->department;
 
-        // $results = Form::user()->user_id;
+        $results = DB::table('users')
+            ->join('form', 'users.user_id', '=', 'form.user_id')
+            ->select('users.*','form.*')
+            ->where('users.department','=',Auth::user()->department)
+            ->get();
 
-        return view('admin/dashboard');
+        return view('admin/dashboard',compact('results'));
     }
 
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,14 +16,43 @@
 
                     You are logged in! as admin
 
-                        <h3>user id: </h3>
-                        @foreach ($results as $result)
-                            <label>{{ $result->user_id }}</label>
-                        @endforeach
+                        
                         
                 </div>
             </div>
         </div>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Employee ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Approved</th>
+                <th scope="col">Approved By</th>
+                <th scope="col">Leave Date</th>
+                <th scope="col">Edit</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($results as $result)
+                    @if ($result->approved == 1)
+                        <tr style="background-color:lightgreen">
+                    @elseif ($result->approved == NULL)
+                        <tr style="background-color:#ff9933">
+                    @else
+                        <tr style="background-color:#f15959">
+                    @endif
+                        <th scope="row">1</th>
+                        <td>{{ $result->user_id }}</td>
+                        <td>{{ $result->f_name.' '.$result->l_name }}</td>
+                        <td>{{ $result->approved}}</td>
+                        <td>{{ $result->approve_by }}</td>
+                        <td>{{ $result->created_at }}</td>
+                        <td><a href="#" class="btn btn-info">Edit</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
