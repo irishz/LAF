8f704ff935a2159d5e194baac586c365474dea1f
Jetsada Prasitsuwan <ice-jetsada@hotmail.com>
2019-04-02 14:08:45 +0700

    special page

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -15,67 +15,69 @@ class AdminController extends Controller
         $manager_dept = Auth::user()->department;
 
 
-        if (Auth::user()->user_id == '1111' | Auth::user()->user_id == '2222') {
+        if (Auth::user()->user_id == '1111' | Auth::user()->user_id == '0226003131') {
             $observes = DB::table('users')
             ->join('form', 'users.user_id', '=', 'form.user_id')
             ->select('users.*','form.*')->get();
 
             return view('admin/dashboard-obs',compact('observes'));
         }else {
-            $results = DB::table('users')
-            ->join('form', 'users.user_id', '=', 'form.user_id')
-            ->select('users.*','form.*')
-            ->where('users.department','=',Auth::user()->department)
-            ->get();
-
-            return view('admin/dashboard',compact('results'));
-        }
-
-        switch ($results->user_id) {
-            //P'PUY acc+itd
-            case '111111':
-                $results = DB::table('users')
-                ->join('form', 'users.user_id', '=', 'form.user_id')
-                ->select('users.*','form.*')
-                // depend on department
-                ->where('users.department','=',Auth::user()->department)
-                ->get();
-
-                return view('admin/extra1',compact('results'));
-                break;
-            //P'pikul opd1+pcu
-            case '222222':
-                $results = DB::table('users')
-                ->join('form', 'users.user_id', '=', 'form.user_id')
-                ->select('users.*','form.*')
-                // depend on department
-                ->where('users.department','=',Auth::user()->department)
-                ->get();
-            
-                return view('admin/extra2',compact('results'));
-                break;
-            //p'pawinee IPD2-5,icu,lnd,ord
-            case '333333':
-                $results = DB::table('users')
-                ->join('form', 'users.user_id', '=', 'form.user_id')
-                ->select('users.*','form.*')
-                // depend on department
-                ->where('users.department','=',Auth::user()->department)
-                ->get();
-
-                return view('admin/extra3',compact('results'));
-                break;
-            //p'wiitatya trd+eng+scu
-            case '444444':
-                $results = DB::table('users')
-                ->join('form', 'users.user_id', '=', 'form.user_id')
-                ->select('users.*','form.*')
-                // depend on department
-                ->where('users.department','=',Auth::user()->department)
-                ->get();
-
-                return view('admin/extra4',compact('results'));
-                break;
+            dd('switch');
+            switch ($results->user_id) {
+                //P'PUY acc+itd
+                case '111111':
+                    $results = DB::table('users')
+                    ->join('form', 'users.user_id', '=', 'form.user_id')
+                    ->select('users.*','form.*')
+                    // depend on department
+                    ->where('users.department','=',Auth::user()->department)
+                    ->get();
+    
+                    return view('admin/extra1',compact('results'));
+                    break;
+                //P'pikul opd1+pcu
+                case '222222':
+                    $results = DB::table('users')
+                    ->join('form', 'users.user_id', '=', 'form.user_id')
+                    ->select('users.*','form.*')
+                    // depend on department
+                    ->where('users.department','=',Auth::user()->department)
+                    ->get();
+                
+                    return view('admin/extra2',compact('results'));
+                    break;
+                //p'pawinee IPD2-5,icu,lnd,ord
+                case '987654321':
+                    $results = DB::table('users')
+                    ->join('form', 'users.user_id', '=', 'form.user_id')
+                    ->select('users.*','form.*')
+                    // depend on department
+                    ->where('users.department','=','')
+                    ->get();
+    
+                    return view('admin/extra3',compact('results'));
+                    break;
+                //p'wiitatya trd+eng+scu
+                case '444444':
+                    $results = DB::table('users')
+                    ->join('form', 'users.user_id', '=', 'form.user_id')
+                    ->select('users.*','form.*')
+                    // depend on department
+                    ->where('users.department','=',Auth::user()->department)
+                    ->get();
+    
+                    return view('admin/extra4',compact('results'));
+                    break;
+                //else
+                default:
+                    $results = DB::table('users')
+                    ->join('form', 'users.user_id', '=', 'form.user_id')
+                    ->select('users.*','form.*')
+                    ->where('users.department','=',Auth::user()->department)
+                    ->get();
+    
+                    return view('admin/dashboard',compact('results'));
+            }
         }
     }
 
