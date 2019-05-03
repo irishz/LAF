cd0178370f9192cef98b75c62b78ab2c89ccd508
Jetsada Prasitsuwan <ice-jetsada@hotmail.com>
2019-05-03 12:50:41 +0700

    create check status manager page

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = User::where('type','=',0)->where('position','หัวหน้าแผนก')->get();
+        $user2 = User::where('type','=',0)->where('position','ผู้จัดการ')->get();
+        $user3 = User::where('type','=',0)->where('position','ผช.ผู้จัดการ')->get();
+        $user4 = User::where('type','=',0)->where('position','รักษาการหัวหน้าแผนก')->get();
+
+        return [$user,$user2,$user3,$user4];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\r  $r
+     * @return \Illuminate\Http\Response
+     */
+    public function show(r $r)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\r  $r
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(r $r)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\r  $r
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, r $r)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\r  $r
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(r $r)
+    {
+        //
+    }
+}
